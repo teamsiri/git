ae4860d15357086a315596ea8f91337477e2776e
teamsiri <siribot466@gmail.com>
2024-07-07 01:48:36 +0300

    Update start.py

--- AnonXMusic/plugins/bot/start.py
+++ AnonXMusic/plugins/bot/start.py
@@ -1,152 +1,173 @@
-import time
-
-from pyrogram import filters
-from pyrogram.enums import ChatType
-from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
-from youtubesearchpython.__future__ import VideosSearch
-
-import config
 from AnonXMusic import app
-from AnonXMusic.misc import _boot_
-from AnonXMusic.plugins.sudo.sudoers import sudoers_list
-from AnonXMusic.utils.database import (
-    add_served_chat,
-    add_served_user,
-    blacklisted_chats,
-    get_lang,
-    is_banned_user,
-    is_on_off,
+from pyrogram import Client, filters
+from pyrogram.types import CallbackQuery, InlineKeyboardButton, InlineKeyboardMarkup, Message
+from config import SUPPORT_GROUP, SUPPORT_CHANNEL, OWNER, START_IMG_URL, BOT_USERNAME
+
+# دالة وهمية add_served_user لتجنب الخطأ
+async def add_served_user(user_id: int):
+   # يمكنك إضافة منطق تخزين المستخدم هنا إذا لزم الأمر
+   pass
+
+@app.on_message(
+   filters.command(["start", "help"]) & filters.private
 )
-from AnonXMusic.utils.decorators.language import LanguageStart
-from AnonXMusic.utils.formatters import get_readable_time
-from AnonXMusic.utils.inline import help_pannel, private_panel, start_panel
-from config import BANNED_USERS
-from strings import get_string
+async def start_(c: Client, message: Message):
+   user_id = message.from_user.id
+   await add_served_user(user_id)
+   await message.reply_photo(
+       photo=START_IMG_URL,
+       caption=f"""اهلا بك انا بۅت اسميي سيري .
+يمڪنني تشغيل الموسيقى في الاتصال .
+ادعم التشغيل في الكروبات والقنوات .
+⎯ ⎯ ⎯ ⎯""",
+       reply_markup=InlineKeyboardMarkup(
+           [
+               [
+                   InlineKeyboardButton(text="‹ : الاوامر : ›", callback_data="command_list")
+               ],[
+                   InlineKeyboardButton(text="‹ : سورس سيري : ›", url=SUPPORT_CHANNEL),
+                   InlineKeyboardButton(text="‹ : مطور البوت : ›", user_id=int(OWNER)),
+               ],
+               [
+                   InlineKeyboardButton(text="‹ : اضف البوت الى مجموعتك : ›", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+               ],
+           ]
+       )
+   )
 
 
-@app.on_message(filters.command(["start"]) & filters.private & ~BANNED_USERS)
-@LanguageStart
-async def start_pm(client, message: Message, _):
-    await add_served_user(message.from_user.id)
-    if len(message.text.split()) > 1:
-        name = message.text.split(None, 1)[1]
-        if name[0:4] == "help":
-            keyboard = help_pannel(_)
-            return await message.reply_photo(
-                photo=config.START_IMG_URL,
-                caption=_["help_1"].format(config.SUPPORT_CHAT),
-                reply_markup=keyboard,
-            )
-        if name[0:3] == "sud":
-            await sudoers_list(client=client, message=message, _=_)
-            if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>sᴜᴅᴏʟɪsᴛ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-                )
-            return
-        if name[0:3] == "inf":
-            m = await message.reply_text("🔎")
-            query = (str(name)).replace("info_", "", 1)
-            query = f"https://www.youtube.com/watch?v={query}"
-            results = VideosSearch(query, limit=1)
-            for result in (await results.next())["result"]:
-                title = result["title"]
-                duration = result["duration"]
-                views = result["viewCount"]["short"]
-                thumbnail = result["thumbnails"][0]["url"].split("?")[0]
-                channellink = result["channel"]["link"]
-                channel = result["channel"]["name"]
-                link = result["link"]
-                published = result["publishedTime"]
-            searched_text = _["start_6"].format(
-                title, duration, views, published, channellink, channel, app.mention
-            )
-            key = InlineKeyboardMarkup(
-                [
-                    [
-                        InlineKeyboardButton(text=_["S_B_8"], url=link),
-                        InlineKeyboardButton(text=_["S_B_9"], url=config.SUPPORT_CHAT),
-                    ],
-                ]
-            )
-            await m.delete()
-            await app.send_photo(
-                chat_id=message.chat.id,
-                photo=thumbnail,
-                caption=searched_text,
-                reply_markup=key,
-            )
-            if await is_on_off(2):
-                return await app.send_message(
-                    chat_id=config.LOGGER_ID,
-                    text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ ᴛᴏ ᴄʜᴇᴄᴋ <b>ᴛʀᴀᴄᴋ ɪɴғᴏʀᴍᴀᴛɪᴏɴ</b>.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-                )
-    else:
-        out = private_panel(_)
-        await message.reply_photo(
-            photo=config.START_IMG_URL,
-            caption=_["start_2"].format(message.from_user.mention, app.mention),
-            reply_markup=InlineKeyboardMarkup(out),
-        )
-        if await is_on_off(2):
-            return await app.send_message(
-                chat_id=config.LOGGER_ID,
-                text=f"{message.from_user.mention} ᴊᴜsᴛ sᴛᴀʀᴛᴇᴅ ᴛʜᴇ ʙᴏᴛ.\n\n<b>ᴜsᴇʀ ɪᴅ :</b> <code>{message.from_user.id}</code>\n<b>ᴜsᴇʀɴᴀᴍᴇ :</b> @{message.from_user.username}",
-            )
+@app.on_callback_query(filters.regex("home_start"))
+async def start_set(_, query: CallbackQuery):
+   await query.answer("الرئيسية")
+   await query.edit_message_text(
+       f"""اهلا بك انا بۅت اسميي سيري .
+يمڪنني تشغيل الموسيقى في الاتصال .
+ادعم التشغيل في الكروبات والقنوات .
+⎯ ⎯ ⎯ ⎯""",
+       reply_markup=InlineKeyboardMarkup(
+           [
+               [
+                   InlineKeyboardButton(text="‹ : الاوامر : ›", callback_data="command_list")
+               ],
+               [
+                   InlineKeyboardButton(text="‹ : سورس سيري : ›", url=SUPPORT_CHANNEL),
+                   InlineKeyboardButton(text="‹ : مطور البوت : ›", user_id=int(OWNER)),
+               ],
+               [
+                   InlineKeyboardButton(text="‹ : اضف البوت الى مجموعتك : ›", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")
+               ],
+           ]
+       )
+   )
 
 
-@app.on_message(filters.command(["start"]) & filters.group & ~BANNED_USERS)
-@LanguageStart
-async def start_gp(client, message: Message, _):
-    out = start_panel(_)
-    uptime = int(time.time() - _boot_)
-    await message.reply_photo(
-        photo=config.START_IMG_URL,
-        caption=_["start_1"].format(app.mention, get_readable_time(uptime)),
-        reply_markup=InlineKeyboardMarkup(out),
-    )
-    return await add_served_chat(message.chat.id)
+@app.on_callback_query(filters.regex("command_list"))
+async def commands_set(_, query: CallbackQuery):
+   user_id = query.from_user.id
+   await query.answer("الاوامر")
+   await query.edit_message_text(
+       f"""اهلا بك عـزيـزي اليك قائمة التحكم .
+استخـدم الازرار بالاسفـل لـ تصفـح اوامـر البوت .
+يمكنك التصال بالمطور اذا كنت تواجه مشكله .
+⎯ ⎯ ⎯ ⎯""",
+       reply_markup=InlineKeyboardMarkup(
+           [
+               [
+                   InlineKeyboardButton("‹ اوامر  التشغيل ›", callback_data="user_command"),
+               ],
+               [
+                   InlineKeyboardButton("‹ اوامر مطوري ›", callback_data="developer_commands"),
+                   InlineKeyboardButton("‹ اوامر المالك ›", callback_data="admin_commands"),
+               ],
+               [
+                   InlineKeyboardButton(" رجوع ", callback_data="home_start"),
+               ],
+           ]
+       )
+   )
 
+@app.on_callback_query(filters.regex("user_command"))
+async def guide_set(_, query: CallbackQuery):
+   await query.answer("قائمــة اوامــر الـتشغـيـل")
+   await query.edit_message_text(
+       f"""طريقة التشغيل ، تابع في الاسفل ↓
 
-@app.on_message(filters.new_chat_members, group=-1)
-async def welcome(client, message: Message):
-    for member in message.new_chat_members:
-        try:
-            language = await get_lang(message.chat.id)
-            _ = get_string(language)
-            if await is_banned_user(member.id):
-                try:
-                    await message.chat.ban_member(member.id)
-                except:
-                    pass
-            if member.id == app.id:
-                if message.chat.type != ChatType.SUPERGROUP:
-                    await message.reply_text(_["start_4"])
-                    return await app.leave_chat(message.chat.id)
-                if message.chat.id in await blacklisted_chats():
-                    await message.reply_text(
-                        _["start_5"].format(
-                            app.mention,
-                            f"https://t.me/{app.username}?start=sudolist",
-                            config.SUPPORT_CHAT,
-                        ),
-                        disable_web_page_preview=True,
-                    )
-                    return await app.leave_chat(message.chat.id)
+شغل + اسم الاغنية او رابط الاغنية
+- لــ تـشـغـيل اغـنـيـة فـي الـمكـالـمـة الـصـوتـيـة .
 
-                out = start_panel(_)
-                await message.reply_photo(
-                    photo=config.START_IMG_URL,
-                    caption=_["start_3"].format(
-                        message.from_user.first_name,
-                        app.mention,
-                        message.chat.title,
-                        app.mention,
-                    ),
-                    reply_markup=InlineKeyboardMarkup(out),
-                )
-                await add_served_chat(message.chat.id)
-                await message.stop_propagation()
-        except Exception as ex:
-            print(ex)
+فيديو + اسم المقـطـع او رابط المقـطـع
+- لــ تـشـغـيل فيـديـو فـي الـمكـالـمـة المـرئيـة .
+
+يوت + الاسـم
+- لـ تحميـل الاغانـي والمقـاطـع الصوتيـه مـن اليوتيـوب .
+⎯ ⎯ ⎯ ⎯
+""",
+       reply_markup=InlineKeyboardMarkup(
+           [
+               [
+                   InlineKeyboardButton("رجوع", callback_data="command_list")
+               ],
+           ]
+       ),
+   )
+
+@app.on_callback_query(filters.regex("admin_commands"))
+async def guide_set(_, query: CallbackQuery):
+   await query.answer("  قائمــة اوامــر الادمــن")
+   await query.edit_message_text(
+       f"""
+-  قائمــة اوامــر الادمــن .
+
+ايقاف / انهاء / اسكت
+- لـ إيقـاف تـشغـيـل الـمـوسـيـقـى فـي المكـالمـة
+
+تخطي
+- لـ تخطـي الاغنيـة وتشغيـل الاغنيـة التاليـه
+
+⎯ ⎯ ⎯ ⎯
+""",
+       reply_markup=InlineKeyboardMarkup(
+           [
+               [
+                   InlineKeyboardButton("رجوع", callback_data="command_list")
+               ],
+           ]
+       ),
+   )
+
+@app.on_callback_query(filters.regex("developer_commands"))
+async def guide_set(_, query: CallbackQuery):
+   if query.from_user.id == int(OWNER):
+       await query.answer("اوامر المطورين")
+       await query.edit_message_text(
+           f"""اوامر المطورين ↓
+
+/addsudo - اضافة مطور
+/delsudo - حذف مطور
+/reboot - إعادة تشغيل البوت
+/update - للتحديث الملفات
+/speedtest - سرعة الخادم
+/logger [disable | enable] معرفة من يستخدم البوت
+/blacklistchat + معرف القناة او المجموعة لحظرها
+/whitelistchat + معرف القناة او المجموعة لرفع حظرها
+/blacklistedchat - المجموعات والقنوات المحظورة
+/block + معرف الشخص لحظره
+/unblock + معرف الشخص لرفع حظره
+/blockedusers - المحظورين
+/gban + معرف الشخص لحظره عام
+/ungban + معرف الشخص لالغاء حظره عام
+/gbannedusers - المحظورين عام
+/broadcast -user -assistant -pin + الرسالة لأذاعتها
+⎯ ⎯ ⎯ ⎯
+""",
+           reply_markup=InlineKeyboardMarkup(
+               [
+                   [
+                       InlineKeyboardButton("رجوع", callback_data="command_list")
+                   ],
+               ]
+           ),
+       )
+   else:
+
+       await query.answer("لا يمكنك الوصول إلى هذا الزر لأنك لست المطور الرئيسي للبوت.", show_alert=True)
