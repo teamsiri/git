27817c656d2b39fd0380ad5182bc446f9cda7555
teamsiri <siribot466@gmail.com>
2024-07-07 01:11:30 +0300

    Update song.py

--- AnonXMusic/plugins/tools/song.py
+++ AnonXMusic/plugins/tools/song.py
@@ -91,7 +91,7 @@ async def song(_, message: Message):
                 audio_file = ydl.prepare_filename(info_dict)
                 ydl.process_info(info_dict)
 
-            rep = f"**• by :** {message.from_user.first_name if message.from_user else 'Freedom'} \n⎯ ⎯ ⎯ ⎯\n• ch : @{Muntazer} ."
+            rep = f"**• by :** {message.from_user.first_name if message.from_user else 'Freedom'} \n⎯ ⎯ ⎯ ⎯\n• ch : @{CHANNEL_SUDO} ."
 
             secmul, dur, dur_arr = 1, 0, duration.split(":")
             for i in range(len(dur_arr) - 1, -1, -1):
